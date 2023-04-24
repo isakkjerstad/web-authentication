3f3c7702853f6728c8eb23a4249e809bb6dd5b9d
Isak <is.kjerstad@hotmail.no>
2023-04-24 13:50:56 +0200

    Added BME680 information
    Description in the database model.

--- server/models.py
+++ server/models.py
@@ -13,7 +13,17 @@ class User(db.Model):
     password_hash = db.Column(db.BINARY(60), nullable = False)
 
 class SensorData(db.Model):
-    ''' Sensor data for a Bosch BME680 sensor. '''
+    '''
+    Sensor data for a Bosch BME680 sensor.
+
+    id          -> unique datapoint ID.
+    time        -> datetime object.
+    location    -> string from sensor describing the sensor placement.
+    temperature -> temperature in degrees celsius (-40 to 85°C).
+    pressure    -> barometric pressure in hectopascals (300 to 1100 hPa).
+    humidity    -> relative humidity in percent (0 to 100%).
+    aqi         -> Air Quality Index, abstract number.
+    '''
 
     id = db.Column(db.Integer, primary_key = True)
     time = db.Column(db.DateTime(timezone = True), server_default = func.now())
